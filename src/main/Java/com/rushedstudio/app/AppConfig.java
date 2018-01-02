db63fc0bfdc8261c4cd32e065dc39e952896ad13
ChristoferRush <poskrz@gmail.com>
2018-01-02 23:50:31 +0100

    ViewResolver

--- src/main/Java/com/rushedstudio/app/AppConfig.java
+++ src/main/Java/com/rushedstudio/app/AppConfig.java
@@ -7,7 +7,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import javax.persistence.EntityManagerFactory;
 
@@ -31,5 +33,10 @@ public class AppConfig {
         return jpaTransactionManager;
     }
 
-
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
 }
