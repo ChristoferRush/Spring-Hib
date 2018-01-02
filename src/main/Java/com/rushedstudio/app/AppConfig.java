427453581fb68f66a086d119c769df8bc16c2197
ChristoferRush <poskrz@gmail.com>
2018-01-02 23:45:44 +0100

    JpaTransactionManager

--- src/main/Java/com/rushedstudio/app/AppConfig.java
+++ src/main/Java/com/rushedstudio/app/AppConfig.java
@@ -4,9 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.persistence.EntityManagerFactory;
 
 @Configuration
 @EnableWebMvc
@@ -17,8 +20,16 @@ public class AppConfig {
 
     @Bean
     public LocalEntityManagerFactoryBean entityManagerFactoryBean(){
-        LocalEntityManagerFactoryBean emfb = new LocalEntityManagerFactoryBean();
-        emfb.setPersistenceUnitName("booksPersistenceUnit");
-        return emfb;
+        LocalEntityManagerFactoryBean entityManagerFactoryBean = new LocalEntityManagerFactoryBean();
+        entityManagerFactoryBean.setPersistenceUnitName("booksPersistenceUnit");
+        return entityManagerFactoryBean;
     }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory){
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager(entityManagerFactory);
+        return jpaTransactionManager;
+    }
+
+
 }
